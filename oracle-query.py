4c944dd7a030de265bd8d686895aaf5996c736ee
nickmonroe123 <153537187+nickmonroe123@users.noreply.github.com>
2024-12-05 12:19:56 -0600

    Update oracle-query.py

--- oracle-query.py
+++ oracle-query.py
@@ -1,132 +1,142 @@
-import msgspec
-from structs_full import FullIdentifier
+class AccountProcess:
+    """Valuable Links to Overall Process
+    Account Identification Process: https://chalk.charter.com/display/ITDMIG/Account+Identification+and+Hydration
+    getSPCAccountv1x1 shown in process above: https://chalk.charter.com/display/EWS/getSpcAccountDivision+V+1.1#
+    findAccountx1x0 shown in process above: https://chalk.charter.com/display/EWS/findAccount+V+1.0
+    """
 
+    # Spectrum Core
+    SPECTRUM_CORE_API = "https://spectrumcoreuat.charter.com/spectrum-core/services/account/ept"
+    SPECTRUM_CORE_ACCOUNT = f"{SPECTRUM_CORE_API}/getSpcAccountDivisionV1x1"
+    SPECTRUM_CORE_BILLING = f"{SPECTRUM_CORE_API}/findAccountV1x0"
+    SPECTRUM_CORE_SYSTEM_ID = "ComplianceService"
 
-class IdentifiedAccount(FullIdentifier):
-    match_score: float = 0.0
-    account_type: str = ""
-    status: str = ""
-    source: str = ""
-    ucan: str = ""
-    billing_account_number: str = ""
-    spectrum_core_account: str = ""
-    spectrum_core_division: str = ""
+    system_id = SPECTRUM_CORE_SYSTEM_ID
+    url_accounts = SPECTRUM_CORE_ACCOUNT
+    url_billing = SPECTRUM_CORE_BILLING
 
+    # This list encompasses the f irst 5 api calls to getSpcAccountDivisionV1x1 (4) and findAccountV1x0 (1)
+    core_services_list = []
 
-class GeneralRequest(msgspec.Struct):
-    country_code: str = msgspec.field(name="countryCode", default = "")
-    phone_number: str = msgspec.field(name="primaryPhone", default = "")
-    first_name: str = msgspec.field(name="firstName", default = "")
-    last_name: str = msgspec.field(name="lastName", default = "")
-    zipcode5: str = msgspec.field(name="zipCode5", default = "")
-    email_address: str = msgspec.field(name="emailAddress", default = "")
-    street_number: str = msgspec.field(name="streetNumber", default = "")
-    street_name: str = msgspec.field(name="streetName", default = "")
-    city: str = msgspec.field(name="city", default = "")
-    state: str = msgspec.field(name="state", default = "")
-    apartment: str = msgspec.field(name="line2", default = "")
+    # This list encompasses the queries against the oracle des database
+    oracle_des_list = []
 
+    # External vs Internal Record
+    ext_request = GeneralRequest()
+    int_record = InternalRecord()
 
-class Description(msgspec.Struct):
-    description: str = ""
+    # For nicknames and such:
+    name_dict = constants.NICKNAMES
 
+    # For street acronyms
+    street_dict = constants.STREET_ACRO
 
-class InternalRecord(GeneralRequest):
-    ucan: str = msgspec.field(name="uCAN", default = "")
-    division_id: str = msgspec.field(name="divisionID", default = "")
-    account_status: str = msgspec.field(name="accountStatus", default = "")
-    secondary_number: str = msgspec.field(name="secondaryPhone", default = "")
-    account_number: str = msgspec.field(name="accountNumber", default = "")
-    account_description: str = ""
-    source: str = msgspec.field(name="sourceMSO", default = "")
-    full_address: str = ""
-    full_address_no_apt: str = ""
-    _account_type: Description = msgspec.field(name="accountType", default = Description)
-    _address_line_1: str = msgspec.field(name="addressLine1", default = "")
-    _address_line_2: str = msgspec.field(name="addressLine2", default = "")
+    # Keep Track of the Fuzzy Amount
+    fuzzy_amount = 0
 
-    def __post_init__(self):
+    # Bad phone number list
+    bad_numbers = constants.BAD_NUMBERS
+
+    ###########################################################################################################
+    # Here is the process for parsing out the request information/discovered information
+    def ext_request_init(self, search_input: FullIdentifier):
+
+        self.ext_request.phone_number = self.phone_parse(search_input.phone_number)
+        (
+            self.ext_request.first_name,
+            self.ext_request.last_name,
+            self.ext_request.zipcode5,
+        ) = self.zip_name_parse(search_input)
+        self.ext_request.email_address = search_input.email
+        (
+            self.ext_request.street_number,
+            self.ext_request.street_name,
+            self.ext_request.city,
+            self.ext_request.state,
+            self.ext_request.apartment,
+        ) = self.address_parse(search_input.address)
+
+    def int_record_init(self, json_dt: dict, msgspec_type: msgspec.Struct):
+        """Parse the data into an internal record type."""
+        self.int_record = msgspec.json.decode(
+            json.dumps(json_dt).encode('utf-8'), type=msgspec_type
+        )
+
+    # This first function will be the retrieval of accounts that have a phone # number match
+    def phone_parse(self, phone_number: PhoneNumber) -> str:
+        num_to_search = (
+            f"{phone_number.area_code}{phone_number.exchange}{phone_number.line_number}"
+        )
+
+        # Error is raised if the regular expression fails (remove all non-digits)
         try:
-            self.street_number, self.street_name = self._address_line_1.split(' ')[0], self._address_line_1
-        except:
-            self.street_number, self.street_name = "", ""
-        self.account_description = self._account_type.description
-        if self._address_line_2 == "":
-            self.full_address = self.street_name + " " + self.city + " " + self.state
+            digits = re.sub(r'\D', '', num_to_search)
+        except re.error as e:
+            logger.info(f"Invalid pattern: {e}")
+            return ''
+
+        return digits[-10:]
+
+    def zip_name_parse(self, search_input: FullIdentifier) -> Tuple[str, str, str]:
+        zipcode = search_input.address.postal_code
+        # Deal with cases of 62002-2131 vs just 62002
+        zipcode = zipcode.split('-')[0]
+        first_name = search_input.name.first_name
+        last_name = search_input.name.last_name
+        return first_name, last_name, zipcode
+
+    def address_parse(self, address: Address) -> Tuple[str, str, str, str, str]:
+        # Pull out the starting element with digits as street number and anything else as street name
+        street_matches = re.search('^\s*(\d[^\s]*)?\s*(.*)?', address.line1)
+        if street_matches:
+            street_name = (street_matches.group(2) or '').strip()
+            street_number = (street_matches.group(1) or '').strip()
         else:
-            self.full_address = self.street_name + " " + self._address_line_2 + " " + self.city + " " + self.state
-            self.full_address_no_apt = self.street_name + " " + self.city + " " + self.state
-
-
-class OracleDESRecord(msgspec.Struct):
-    account_number: str = msgspec.field(name="ACCT_NUM", default = "")
-    phone_number: str = msgspec.field(name="PRIMARY_NUMBER", default = "")
-    secondary_number: str = msgspec.field(name="secondaryPhone", default = "")
-    first_name: str = msgspec.field(name="ACCT_NAME", default = "")
-    last_name: str = ""
-    zipcode5: str = msgspec.field(name="PSTL_CD_TXT_BLR", default = "")
-    email_address: str = msgspec.field(name="EMAIL_ADDR", default = "")
-    street_number: str = ""
-    street_name: str = ""
-    city: str = msgspec.field(name="CITY_NM_BLR", default = "")
-    state: str = msgspec.field(name="STATE_NM_BLR", default = "")
-    ucan: str = msgspec.field(name="UCAN", default = "")
-    division_id: str = msgspec.field(name="SPC_DIV_ID", default = "")
-    account_status: str = msgspec.field(name="ACCOUNTSTATUS", default = "")
-    account_description: str = msgspec.field(name="ACCT_TYPE_CD", default = "")
-    source: str = msgspec.field(name="SRC_SYS_CD", default = "")
-    full_address: str = ""
-    full_address_no_apt: str = ""
-    _address_line_1: str = msgspec.field(name="BLR_ADDR1_LINE", default = "")
-    _address_line_2: str = msgspec.field(name="BLR_ADDR2_LINE", default = "")
-
-    def __post_init__(self):
+            street_name = ''
+            street_number = ''
+        city = address.city
+        state = address.state
+        apartment = address.line2
+        return street_number, street_name, city, state, apartment
+def full_search(self, search_input: FullIdentifier) -> list[IdentifiedAccount]:
         try:
-            self.street_number, self.street_name = self._address_line_1.split(' ')[0], self._address_line_1
-        except:
-            self.street_number, self.street_name = "", ""
-        try:
-            self.first_name, self.last_name = self.first_name.split(',')[1], self.first_name.split(',')[0]
-        except:
-            self.first_name, self.last_name = "", ""
-        if self._address_line_2 == "":
-            self.full_address = self.street_name + " " + self.city + " " + self.state
-        else:
-            self.full_address = self.street_name + " " + self._address_line_2 + " " + self.city + " " + self.state
-            self.full_address_no_apt = self.street_name + " " + self.city + " " + self.state
-import msgspec
+            # Log the request to start so we have a record of its existence
+            logger.info('This is the start of a new account identification process.')
+            logger.info('Requestor info:')
+            logger.info(search_input)
 
+            # Fill out account_info dictionary, this will parse the data for all of the needed columns
+            self.ext_request_init(search_input)
 
-class Name(msgspec.Struct):
-    first_name: str
-    last_name: str
-    middle_name: str = ""
-    suffix: str = ""
-    prefix: str = ""
+            # Search the spectrum core services api for phone account matches
+            self.phone_search()
+            # Search the spectrum core services api for name zip combo matches
+            self.name_zip_search()
+            # Search the spectrum core services api for email account matches
+            self.email_search()
+            # Search the spectrum core services api for address matches
+            self.address_search()
 
+            # Make the final call out to the finalAccountv1x0
+            self.billing_search()
+            # Call fuzzy match for the 1 billing api call
+            self.core_services_list = self.confirmed_matches(
+                self.core_services_list, "sc"
+            )
 
-class PhoneNumber(msgspec.Struct):
-    country_code: str = ""
-    area_code: str = ""
-    exchange: str = ""
-    line_number: str = ""
-    extension: str = ""
-    type_code: str = ""
+            # Run through the oracle des process
+            self.oracle_des_process()
+            # Call fuzzy match for the oracle des process
+            self.core_services_list += self.confirmed_matches(
+                self.oracle_des_list, "oracle"
+            )
 
+            # CLean the dataset to get distinct account numbers for the first 1 billing api call to core services
+            self.clean_core_services_list()
 
-class Address(msgspec.Struct):
-    city: str
-    state: str
-    line1: str
-    postal_code: str
-    line2: str = ""
-    country_code: str = ""
-
-
-class FullIdentifier(msgspec.Struct):
-    """Combine name, address, phone number, and email."""
-
-    name: Name
-    phone_number: PhoneNumber
-    address: Address
-    email: str
+            logger.info('This is the end of an account identification process.')
+            logger.info('\n')
+            return self.create_output_list()
+            # return self.core_services_list
+        except Exception as e:
+            print(f"Error in the API process: {e}")

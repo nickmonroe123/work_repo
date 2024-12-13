4200d72771e0140df13fb37b90a9d409d6b5ba26
nickmonroe123 <153537187+nickmonroe123@users.noreply.github.com>
2024-12-13 13:41:19 -0600

    Update oracle-query.py

--- oracle-query.py
+++ oracle-query.py
@@ -1,10 +1,11 @@
-class TestHelperFunctions(TestCase):
-    def test_lower_no_spaces(self):
-        self.assertEqual(lower_no_spaces("Hello World"), "helloworld")
-        self.assertEqual(lower_no_spaces("  Spaces  "), "spaces")
-        self.assertEqual(lower_no_spaces(""), "")
-
-    def test_normalize_address(self):
-        self.assertEqual(normalize_address("123 ST Main ST"), "123streetmainstreet")
-        self.assertEqual(normalize_address("45 AVE First"), "45avenuefirst")
-        self.assertEqual(normalize_address(""), "")
+from account_identification.services import (
+    AccountProcess,
+    connect_to_oracle,
+    query_with_params,
+    search_with_phone,
+    search_with_address,
+    search_with_email,
+    search_with_zip_name,
+    normalize_address,
+    lower_no_spaces
+)

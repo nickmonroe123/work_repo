702da5ba0f04f78366f3f825f696b90149892582
nickmonroe123 <153537187+nickmonroe123@users.noreply.github.com>
2024-12-16 09:38:03 -0600

    Update oracle-query.py

--- oracle-query.py
+++ oracle-query.py
@@ -1,27 +1,70 @@
-    def clean_output_list(self) -> list[IdentifiedAccount]:
-        sorted_data: list[IdentifiedAccount] = sorted(
-            self.output_list, key=itemgetter('match_score'), reverse=True
-        )
-        # Remove duplicates based on the fourth item
-        unique_data = []
-        seen = set()
-        for item in sorted_data:
-            acct_num = item.billing_account_number
-            if acct_num not in seen and acct_num != '':
-                unique_data.append(item)
-                seen.add(acct_num)
+class IdentifyAccountsView(MsgSpecAPIView):
+    def post(self, request, *args, **kwargs):
+        search_input = msgspec.json.decode(request.data, type=FullIdentifier)
+        results = identify_accounts(search_input=search_input)
+        return Response(data=results)
 
-        return unique_data
 
-class IdentifiedAccount(FullIdentifier):
-    match_score: float = 0.0
-    account_type: str = ""
-    status: str = ""
-    source: str = ""
-    ucan: str = ""
-    billing_account_number: str = ""
-    spectrum_core_account: str = ""
-    spectrum_core_division: str = ""
+import re
 
-TypeError at /account_identification/api/
-'IdentifiedAccount' object is not subscriptable
+import msgspec
+
+
+class Name(msgspec.Struct):
+    first_name: str
+    last_name: str
+    middle_name: str = ""
+    suffix: str = ""
+    prefix: str = ""
+
+
+class PhoneNumber(msgspec.Struct):
+    country_code: str = ""
+    area_code: str = ""
+    exchange: str = ""
+    line_number: str = ""
+    extension: str = ""
+    type_code: str = ""
+
+    @property
+    def ten_digits_only(self) -> str:
+        starting_number = f"{self.area_code}{self.exchange}{self.line_number}"
+        digits_only = re.sub(r"\D", "", starting_number)
+        return digits_only[-10:]
+
+
+class Address(msgspec.Struct):
+    city: str
+    state: str
+    line1: str
+    postal_code: str
+    line2: str = ""
+    country_code: str = ""
+
+    def _street_name_and_number(self) -> tuple[str, str]:
+        street_matches = re.search(r'^\s*(\d[^\s]*)?\s*(.*)?', self.line1)
+        street_name = (street_matches.group(2) or '').strip()
+        street_number = (street_matches.group(1) or '').strip()
+        return street_name, street_number
+
+    @property
+    def street_name(self):
+        return self._street_name_and_number()[0]
+
+    @property
+    def street_number(self):
+        return self._street_name_and_number()[1]
+
+    @property
+    def simplified_postal_code(self):
+        """First portion of a dash-separated postal code."""
+        return self.postal_code.split("-")[0]
+
+
+class FullIdentifier(msgspec.Struct):
+    """Combine name, address, phone number, and email."""
+
+    name: Name
+    phone_number: PhoneNumber
+    address: Address
+    email: str

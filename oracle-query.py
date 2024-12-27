58d55285b1ff204313722f9b7372b03c2336ab68
nickmonroe123 <153537187+nickmonroe123@users.noreply.github.com>
2024-12-27 10:47:30 -0600

    Update oracle-query.py

--- oracle-query.py
+++ oracle-query.py
@@ -1,139 +1,88 @@
-{'expand': 'schema,names', 'startAt': 0, 'maxResults': 2, 'total': 5, 'issues': [{'expand': 'operations,versionedRepresentations,editmeta,changelog,renderedFields', 'id': '12687855', 'self': 'https://jira-uat.corp.chartercom.com/rest/api/2/issue/12687855', 'key': 'DPCP-2879', 'fields': {'issuetype': {'self': 'https://jira-uat.corp.chartercom.com/rest/api/2/issuetype/6', 'id': '6', 'description': 'Created by Jira Software - do not edit or delete. Issue type for a big user story that needs to be broken down.', 'iconUrl': 'https://jira-uat.corp.chartercom.com/secure/viewavatar?size=xsmall&avatarId=17107&avatarType=issuetype', 'name': 'Epic', 'subtask': False, 'avatarId': 17107}, 'resolutiondate': None, 'status': {'self': 'https://jira-uat.corp.chartercom.com/rest/api/2/status/10537', 'description': '', 'iconUrl': 'https://jira-uat.corp.chartercom.com/images/icons/statuses/generic.png', 'name': 'New', 'id': '10537', 'statusCategory': {'self': 'https://jira-uat.corp.chartercom.com/rest/api/2/statuscategory/2', 'id': 2, 'key': 'new', 'colorName': 'default', 'name': 'To Do'}}}}, {'expand': 'operations,versionedRepresentations,editmeta,changelog,renderedFields', 'id': '12687167', 'self': 'https://jira-uat.corp.chartercom.com/rest/api/2/issue/12687167', 'key': 'DPCP-2878', 'fields': {'issuetype': {'self': 'https://jira-uat.corp.chartercom.com/rest/api/2/issuetype/6', 'id': '6', 'description': 'Created by Jira Software - do not edit or delete. Issue type for a big user story that needs to be broken down.', 'iconUrl': 'https://jira-uat.corp.chartercom.com/secure/viewavatar?size=xsmall&avatarId=17107&avatarType=issuetype', 'name': 'Epic', 'subtask': False, 'avatarId': 17107}, 'resolutiondate': None, 'status': {'self': 'https://jira-uat.corp.chartercom.com/rest/api/2/status/10537', 'description': '', 'iconUrl': 'https://jira-uat.corp.ey': 'new', 'colorName': 'default', 'name': 'To Do'}}}}]}: 'New', 'id': '10537', 'statusCategory': {'self': 'https://jira-uat.corp.chartercom.com/rest/api/2/statuscategory/2', 'id': 2, 'k
+from typing import Dict, List, Optional
+from datetime import datetime
+from django.utils.timezone import make_aware
 
-{'expand': 'schema,names', 'startAt': 0, 'maxResults': 2, 'total': 4, 'issues': [{'expand': 'operations,versionedRepresentations,editmeta,changelog,renderedFields', 'id': '12688458', 'self': 'https://jira-uat.corp.chartercom.com/rest/api/2/issue/12688458', 'key': 'DPCP-2884', 'fields': {'issuetype': {'self': 'https://jira-uat.corp.chartercom.com/rest/api/2/issuetype/7', 'id': '7', 'description': 'Created by Jira Software - do not edit or delete. Issue type for a user story.', 'iconUrl': 'https://jira-uat.corp.chartercom.com/secure/viewavatar?size=xsmall&avatarId=17115&avatarType=issuetype', 'name': 'Story', 'subtask': False, 'avatarId': 17115}, 'resolutiondate': None, 'status': {'self': 'https://jira-uat.corp.chartercom.com/rest/api/2/status/10537', 'description': '', 'iconUrl': 'https://jira-uat.corp.chartercom.com/images/icons/statuses/generic.png', 'name': 'New', 'id': '10537', 'statusCategory': {'self': 'https://jira-uat.corp.chartercom.com/rest/api/2/statuscategory/2', 'id': 2, 'key': 'new', 'colorName': 'default', 'name': 'To Do'}}}}, {'expand': 'operations,versionedRepresentations,editmeta,changelog,renderedFields', 'id': '12687856', 'self': 'https://jira-uat.corp.chartercom.com/rest/api/2/issue/12687856', 'key': 'DPCP-2880', 'fields': {'issuetype': {'self': 'https://jira-uat.corp.chartercom.com/rest/api/2/issuetype/7', 'id': '7', 'description': 'Created by Jira Software - do not edit or delete. Issue type for a user story.', 'iconUrl': 'https://jira-uat.corp.chartercom.com/secure/viewavatar?size=xsmall&avatarId=17115&avatarType=issuetype', 'name': 'Story', 'subtask': False, 'avatarId': 17115}, 'resolutiondate': None, 'status': {'self': 'https://jira-uat.corp.chartercom.com/rest/api/2/status/10537', 'description': '', 'iconUrl': 'https://jira-uat.corp.chartercom.com/images/icons/statuses/generic.png', 'name': 'New', 'id': '10537', 'statusCategory': {'self': 'https://jira-uat.corp.chartercom.com/rest/api/2/statuscategory/2', 'id': 2, 'key': 'new', 'colorName': 'default', 'name': 'To Do'}}}}]}
-
-{'expand': 'schema,names', 'startAt': 0, 'maxResults': 2, 'total': 67, 'issues': [{'expand': 'operations,versionedRepresentations,editmeta,changelog,renderedFields', 'id': '12687170', 'self': 'https://jira-uat.corp.chartercom.com/rest/api/2/issue/12687170', 'key': 'DPCP-2882', 'fields': {'issuetype': {'self': 'https://jira-uat.corp.chartercom.com/rest/api/2/issuetype/72503', 'id': '72503', 'description': '', 'iconUrl': 'https://jira-uat.corp.chartercom.com/secure/viewavatar?size=xsmall&avatarId=17100&avatarType=issuetype', 'name': 'Change Ticket', 'subtask': False, 'avatarId': 17100}, 'customfield_16748': {'self': 'https://jira-uat.corp.chartercom.com/rest/api/2/customFieldOption/3007660', 'value': 'Change Valid', 'id': '3007660', 'disabled': False}, 'resolutiondate': '2024-12-17T15:56:52.000-0500', 'status': {'self': 'https://jira-uat.corp.chartercom.com/rest/api/2/status/12438', 'description': '', 'iconUrl': 'https://jira-uat.corp.chartercom.com/images/icons/statuses/generic.png', 'name': 'Completed', 'id': '12438', 'statusCategory': {'self': 'https://jira-uat.corp.chartercom.com/rest/api/2/statuscategory/3', 'id': 3, 'key': 'done', 'colorName': 'success', 'name': 'Done'}}}}, {'expand': 'operations,versionedRepresentations,editmeta,changelog,renderedFields', 'id': '12687169', 'self': 'https://jira-uat.corp.chartercom.com/rest/api/2/issue/12687169', 'key': 'DPCP-2881', 'fields': {'issuetype': {'self': 'https://jira-uat.corp.chartercom.com/rest/api/2/issuetype/72503', 'id': '72503', 'description': '', 'iconUrl': 'https://jira-uat.corp.chartercom.com/secure/viewavatar?size=xsmall&avatarId=17100&avatarType=issuetype', 'name': 'Change Ticket', 'subtask': False, 'avatarId': 17100}, 'customfield_16748': {'self': 'https://jira-uat.corp.chartercom.com/rest/api/2/customFieldOption/3007660', 'value': 'Change Valid',https://jira-uat.corp.chartercom.com/images/icons/statuses/generic.png', 'name': 'Completed', 'id': '12438', 'statusCategory': {'self': 'https://jira-uat.corp.chartercom.com/rest/api/2/statuscategory/3', 'id': 3, 'key': 'done', 'colorName': 'success', 'name': 'Done'}}}}]}
-
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
-class JiraClient:
-    """Class to abstract common API calls to Jira."""
-
-    def __init__(self, base_url: str = None) -> None:
-        self.session = JiraSession(base_url)
-
-    def get_issue(self, issue_id: str) -> dict:
-        """Gets the API response for the given issue.
-
-        Args:
-            issue_id (str): The id for the issue
-
-        Returns:
-            dict: The API response
-        """
-
-        return self.session.get(f'/rest/api/2/issue/{issue_id}')
-
-    def create_issue(self, payload: dict) -> dict:
-        """Creates an issue with the given JSON payload.
-
-        Args:
-            payload (dict): The payload with the parameters for the issue
-
-        Returns:
-            dict: The API response
-        """
-        return self.session.post('/rest/api/2/issue', json=payload)
-
-    def bulk_create_issues(self, payloads: list[dict]) -> dict:
-        """Bulk creates issues from a list of dictionaries.
-
-        Args:
-            payloads (dict): The list of issues (in dict form) to be created
-
-        Returns:
-            dict: The API response
-        """
-        return self.session.post(
-            '/rest/api/2/issue/bulk', json={'issueUpdates': payloads}
-        )
-
-    def close_issue(self, jira_id: str, close_code: str = None) -> dict:
-        """Closes the given Jira ticket. Jira uses a transistion to update the status.
-        More information is doucumented: https://docs.atlassian.com/software/jira/docs/api/REST/7.6.1/#api/2/issue-doTransition
-
-        Args:
-            jira_id (str): The target jira id
-            close_code (str, optional): Task tickets require the close code to be provided. Defaults to None.
-
-        Returns:
-            dict: The API response
-        """
-        payload = {
-            'transition': {'id': constants.CLOSE_TRANSITION_ID},
+class JiraParser:
+    @staticmethod
+    def parse_common_fields(issue: Dict) -> Dict:
+        """Extract common fields from a Jira issue."""
+        fields = issue['fields']
+        return {
+            'id': issue['key'],
+            'status': fields['status']['name'],
+            'issue_type': fields['issuetype']['name'],
+            'close_date': make_aware(datetime.strptime(fields['resolutiondate'], '%Y-%m-%dT%H:%M:%S.%f%z')) 
+                         if fields.get('resolutiondate') else None
         }
-        if close_code is not None:
-            payload['fields'] = {constants.CLOSE_CODE: {'value': close_code}}
-        return self.session.post(
-            f'/rest/api/2/issue/{jira_id}/transitions', json=payload
-        )
 
-    def search_issues(self, jql: str, fields: str, max_results: int = 2, start_at: int = 0):
-        """Search for issues using JQL."""
-        params = {
-            'jql': jql,
-            'maxResults': max_results,
-            'startAt': start_at,
-            'fields': fields
-        }
-        return self.session.get('/rest/api/2/search', params=params).json()
+    @staticmethod
+    def parse_epics(jira_response: Dict) -> List[Dict]:
+        """Parse epic issues from Jira response."""
+        epics = []
+        for issue in jira_response.get('issues', []):
+            if issue['fields']['issuetype']['name'] == 'Epic':
+                epic_data = JiraParser.parse_common_fields(issue)
+                epics.append(epic_data)
+        return epics
 
+    @staticmethod
+    def parse_stories(jira_response: Dict) -> List[Dict]:
+        """Parse story issues from Jira response."""
+        stories = []
+        for issue in jira_response.get('issues', []):
+            if issue['fields']['issuetype']['name'] == 'Story':
+                story_data = JiraParser.parse_common_fields(issue)
+                # You might need to add logic here to link to parent epic
+                stories.append(story_data)
+        return stories
 
-def get_jira_updated_issues(jql: str, fields: str):
-    # Initialize client
-    client = JiraClient()
+    @staticmethod
+    def parse_tasks(jira_response: Dict) -> List[Dict]:
+        """Parse task issues from Jira response."""
+        tasks = []
+        for issue in jira_response.get('issues', []):
+            if issue['fields']['issuetype']['name'] == 'Change Ticket':
+                task_data = JiraParser.parse_common_fields(issue)
+                # Add task-specific fields
+                task_data['close_code'] = (
+                    issue['fields'].get('customfield_16748', {}).get('value')
+                    if issue['fields'].get('customfield_16748') else None
+                )
+                tasks.append(task_data)
+        return tasks
 
-    try:
-        # Retrieve all the epics in the given time range
-        jql_results = client.search_issues(jql, fields)
-        print(jql_results)
-        print("\n")
-        # TODO: Parse down the data (make model/struct)
-        # jql_parsed = parsers.extract_values(jql_results)
-        # TODO: Create the records (issue, epic, task)
+def create_or_update_records(epics_json: Dict, stories_json: Dict, tasks_json: Dict):
+    """Create or update records in the database."""
+    from django.db import transaction
+    
+    with transaction.atomic():
+        # Process epics
+        epic_records = []
+        for epic_data in JiraParser.parse_epics(epics_json):
+            epic, created = Epic.objects.update_or_create(
+                id=epic_data['id'],
+                defaults=epic_data
+            )
+            epic_records.append(epic)
 
-        # update: jira id, close date, close code, status
-        # for record in jql_parsed:
-        #     print(record)
+        # Process stories
+        story_records = []
+        for story_data in JiraParser.parse_stories(stories_json):
+            # You might need to add logic here to determine the parent epic
+            story, created = Story.objects.update_or_create(
+                id=story_data['id'],
+                defaults=story_data
+            )
+            story_records.append(story)
 
-    except requests.exceptions.HTTPError as e:
-        print(f"Error occurred: {e}")
-        if '401' in str(e):
-            print("Authentication failed. Please check your access token.")
-        elif '403' in str(e):
-            print("Permission denied. Please check your access permissions.")
-        else:
-            print("An unexpected error occurred.")
+        # Process tasks
+        task_records = []
+        for task_data in JiraParser.parse_tasks(tasks_json):
+            # You might need to add logic here to determine the parent story
+            task, created = Task.objects.update_or_create(
+                id=task_data['id'],
+                defaults=task_data
+            )
+            task_records.append(task)
 
-from django.db import models
-from utilities.models import TrackedMixin
-
-
-# Base Abstract Model
-class Issue(TrackedMixin):
-    id = models.CharField(primary_key=True, max_length=100)
-    status = models.CharField(max_length=50)
-    issue_type = models.CharField(max_length=50)
-    close_date = models.DateTimeField(null=True, blank=True)
-
-    class Meta:
-        abstract = True
-
-
-# Concrete Models
-class Epic(Issue):
-    """Base Epic Model."""
-
-
-class Story(Issue):
-    """Story level items."""
-
-    parent = models.ForeignKey(Epic, on_delete=models.CASCADE, related_name='stories')
-
-
-class Task(Issue):
-    """Task level items."""
-
-    parent = models.ForeignKey(Story, on_delete=models.CASCADE, related_name='tasks')
-    business_unit = models.CharField(max_length=100)
-    close_code = models.CharField(max_length=100, null=True, blank=True)
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+    return epic_records, story_records, task_records

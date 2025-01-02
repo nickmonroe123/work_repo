1bd0413d2430947be162e60cd91e275456f4f7c5
nickmonroe123 <153537187+nickmonroe123@users.noreply.github.com>
2025-01-02 12:47:52 -0600

    Update oracle-query.py

--- oracle-query.py
+++ oracle-query.py
@@ -1,51 +1,44 @@
-from celery import shared_task
-from . import services
+"""Services for the Jira app."""
+import requests
+
+from jira_integration.session import JiraSession
 from jira_integration import constants
+from jira_integration.parsers import create_or_update_records
 
 
-@shared_task(bind=True)
-# TODO: Will need to remove request once we make it a task
-def fetch_jira_epic_updates(request):
-    """Subtask 1: Fetches updated issues from Jira"""
-    services.get_jira_updated_issues(
-        jql=constants.EPIC_JQL,
-        fields=constants.EPIC_FIELDS,
-        jira_type='epic'
-    )
+class JiraClient:
+    """Class to abstract common API calls to Jira."""
+
+    def __init__(self, base_url: str = None) -> None:
+        # self.session = JiraSession(base_url)
+        self.session = JiraSession()
 
 
-@shared_task(bind=True)
-# TODO: Will need to remove request once we make it a task
-def fetch_jira_story_updates(request):
-    """Subtask 2: Fetches updated stories from Jira"""
-    services.get_jira_updated_issues(
-        jql=constants.STORY_JQL,
-        fields=constants.STORY_FIELDS,
-        jira_type='story'
-    )
+    def search_issues(self, jql: str, fields: str, max_results: int = constants.MAX_JQL_RESULTS, start_at: int = 0):
+        """Search for issues using JQL."""
+        params = {
+            'jql': jql,
+            'maxResults': max_results,
+            'startAt': start_at,
+            'fields': fields
+        }
+        return self.session.get('/rest/api/2/search', params=params).json()
 
 
-@shared_task(bind=True)
-# TODO: Will need to remove request once we make it a task
-def fetch_jira_task_updates(request):
-    """Subtask 3: Fetches updated tasks from Jira"""
-    services.get_jira_updated_issues(
-        jql=constants.TASK_JQL,
-        fields=constants.TASK_FIELDS,
-        jira_type='task'
-    )
+def get_jira_updated_issues(jql: str, fields: str, jira_type: str):
+    # Initialize client
+    client = JiraClient()
 
+    try:
+        # Retrieve all the epics in the given time range
+        jql_results = client.search_issues(jql, fields)
+        create_or_update_records(jql_results, jira_type)
 
-@shared_task(bind=True)
-# TODO: Will need to remove request once we make it a task
-def process_jira_updates_and_changes(request):
-    """
-    In this process we are attempting to remove the webhook process and transition to
-    using the jira JQL api process instead for any updates and story creations.
-    """
-    # Fetch all the jira epics and create them in db
-    fetch_jira_epic_updates()
-    # Fetch all the jira stories and create them in db
-    fetch_jira_story_updates()
-    # Fetch all the jira tasks and create them in db
-    fetch_jira_task_updates()
+    except requests.exceptions.HTTPError as e:
+        print(f"Error occurred: {e}")
+        if '401' in str(e):
+            print("Authentication failed. Please check your access token.")
+        elif '403' in str(e):
+            print("Permission denied. Please check your access permissions.")
+        else:
+            print("An unexpected error occurred.")
